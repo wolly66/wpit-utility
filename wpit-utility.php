123afaeba2f76b856d90478bf4cee63025889c33
wolly66 <wolly66@gmail.com>
2015-06-03 16:52:11 +0200

    deleted my UA code

--- wpit-utility.php
+++ wpit-utility.php
@@ -133,7 +133,7 @@ class Wpit_Utility {
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		ga('create', 'UA-312898-1', 'auto');
+		ga('create', 'UA-xxxxxx-x', 'auto');
 		ga('set', 'anonymizeIp', true);
 		ga('send', 'pageview');
 
